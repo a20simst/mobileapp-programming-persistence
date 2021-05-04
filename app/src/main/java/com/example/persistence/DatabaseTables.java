4d8081213300aa891a0b8489ded62ab6ee5a5c4d
Simon <a20simst@student.his.se>
2021-05-04 11:35:57 +0200

    created class for database table

---
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -0,0 +1,25 @@
+package com.example.persistence;
+
+class DatabaseTables {
+
+    static class personInfo {
+
+        static final String TABLE_NAME = "information";
+        static final String COLUMN_NAME_GENDER = "gender";
+        static final String COLUMN_NAME_NAME = "name";
+        static final String COLUMN_NAME_AGE = "age";
+
+    }
+
+    static final String SQL_CREATE_TABLE_MOUNTAIN =
+            // "CREATE TABLE mountain (id INTEGER PRIMARY KEY, name TEXT, height INT)"
+            "CREATE TABLE " + personInfo.TABLE_NAME + " (" +
+                    personInfo.COLUMN_NAME_GENDER + " TEXT PRIMARY KEY," +
+                    personInfo.COLUMN_NAME_NAME + " TEXT," +
+                    personInfo.COLUMN_NAME_AGE + " INT)";
+
+    static final String SQL_DELETE_TABLE_MOUNTAIN =
+            // "DROP TABLE IF EXISTS mountain"
+            "DROP TABLE IF EXISTS " + personInfo.TABLE_NAME;
+
+}
