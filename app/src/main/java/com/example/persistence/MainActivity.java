9b6c6b13e681d1c78859912bc6249d598987bc16
Simon <a20simst@student.his.se>
2021-05-04 11:45:36 +0200

    Added button, edittext, textview variables

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
+    Button read = findViewById(R.id.button1);
+    Button write = findViewById(R.id.button2);
+    EditText gender = findViewById(R.id.edit1);
+    EditText name = findViewById(R.id.edit2);
+    EditText age = findViewById(R.id.edit3);
+    TextView showtext = findViewById(R.id.inserted_text);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
