cea01f052dec50e73cbc3c9dcf5186fb4b589b14
Simon <a20simst@student.his.se>
2021-05-06 11:26:49 +0200

    Added new version to database and removed primary key

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -14,8 +14,8 @@ class DatabaseTables {
     static final String SQL_CREATE_TABLE_PERSON =
             // "CREATE TABLE mountain (id INTEGER PRIMARY KEY, name TEXT, height INT)"
             "CREATE TABLE " + personInfo.TABLE_NAME + " (" +
-                    personInfo.COLUMN_NAME_GENDER + " TEXT PRIMARY KEY," +
-                    personInfo.COLUMN_NAME_NAME + " TEXT," +
+                    personInfo.COLUMN_NAME_GENDER + " TEXT ," +
+                    personInfo.COLUMN_NAME_NAME + " TEXT ," +
                     personInfo.COLUMN_NAME_AGE + " INT)";
 
     static final String SQL_DELETE_TABLE_PERSON =
